59ffe4d29c7ed0dc9d52383a2623759b5d4e490c
Azure Training <training.az400@hotmail.com>
2023-02-28 13:36:10 +0300

    version 3

--- client/src/App.js
+++ client/src/App.js
@@ -18,7 +18,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React - Version 2
+            Learn React - Version 3
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
